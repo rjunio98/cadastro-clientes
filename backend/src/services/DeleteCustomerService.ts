96065cfe4bfb985bcfdc33b5a9f80a611e3a75c0
rjunio98 <robledorjunior1@gmail.com>
2024-02-15 08:01:17 -0300

    Adiciona controlador e serviço para exclusão de cliente

---
+++ backend/src/services/DeleteCustomerService.ts
@@ -0,0 +1,33 @@
+import prismaClient from "../prisma";
+
+interface DeleteCustomerProps {
+  id: string;
+}
+
+class DeleteCustomerService {
+  async execute({ id }: DeleteCustomerProps) {
+    if (!id) {
+      throw new Error("Id is required");
+    }
+
+    const findCustomer = await prismaClient.customer.findFirst({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!findCustomer) {
+      throw new Error("Customer not found");
+    }
+
+    await prismaClient.customer.delete({
+      where: {
+        id: findCustomer.id,
+      },
+    });
+
+    return { message: "Customer deleted successfully" };
+  }
+}
+
+export { DeleteCustomerService };
