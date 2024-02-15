a9251da4ea83fcd0fd15451d560f92a2edf1ccbf
rjunio98 <robledorjunior1@gmail.com>
2024-02-15 07:36:15 -0300

    Adiciona rota para criação de cliente

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,11 +5,23 @@ import {
   FastifyReply,
 } from "fastify";
 
+import { CreateCustomerController } from "./controllers/CreateCustomerController";
+
 export async function routes(
   fastify: FastifyInstance,
   options: FastifyPluginOptions
 ) {
-  fastify.get("/teste", async (request: FastifyRequest, reply: FastifyReply) => {
-    return { hello: "world"};
-  });
+  fastify.get(
+    "/teste",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      return { hello: "world" };
+    }
+  );
+
+  fastify.post(
+    "/customer",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      return new CreateCustomerController().handle(request, reply);
+    }
+  );
 }
